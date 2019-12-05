fda2f5683f22c6f7516806e6ab442c872eeba2e4
Tuan Truong <tuan.truong625@gmail.com>
2019-12-04 22:22:56 -0500

    Install firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/tailwind.css'
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBNPDg0u0IDBHcBpOVNhkQRG4jFPvG6jdI",
+    authDomain: "recipe-book-3057a.firebaseapp.com",
+    databaseURL: "https://recipe-book-3057a.firebaseio.com",
+    projectId: "recipe-book-3057a",
+    storageBucket: "recipe-book-3057a.appspot.com",
+    messagingSenderId: "43863100586",
+    appId: "1:43863100586:web:7fbaab162f5e14cb3ed4d3",
+    measurementId: "G-6KJC8TFHN9"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 
 Vue.config.productionTip = false
