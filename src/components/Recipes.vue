481b97a4a9f1752f9dae5f0adb5d791cdfb3a30d
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2019-12-12 17:59:47 -0500

    fixing redundancies

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -3,20 +3,12 @@
     <navigation/>
     <h1 class="text-6xl">Recipes</h1>
     <div v-for="recipe in recipes" :key="recipe.id">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img class="w-full" :src="recipe.imageURL" alt="Sunset in the mountains">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{{ recipe.name }}</div>
-          <p class="text-gray-700 text-base">
-            {{ recipe.ingredients[0].name }}:
-            {{ recipe.ingredients[0].quantity }}
-            {{ recipe.imageURL }}
+      <pre>
+            <code>
+            {{ recipe }}
+            </code>
 
-          </p>
-        </div>
-
-      </div>
-
+            </pre>
     </div>
   </section>
 </template>
